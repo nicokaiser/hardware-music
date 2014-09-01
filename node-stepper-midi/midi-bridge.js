ffc8811e1f8294858422d0346f9ba25c6abc295d
Nico Kaiser <nico@kaiser.me>
2014-09-01 22:42:39 +0200

    Add a simple midi-bridge

---
+++ node-stepper-midi/midi-bridge.js
@@ -0,0 +1,82 @@
+'use strict';
+
+var midi = require('midi');
+var MIDIUtils = require('midiutils');
+var StepperPlayer = require('./lib/stepper-player').StepperPlayer;
+
+
+// Configuration
+
+var midiPort = 1; // real MIDI port (or false)
+var midiName = 'Hardware Music'; // "Hardware Music"
+var device = '/dev/ttyACM0'; // /dev/ttyACM0
+
+
+var stepperPlayer;
+
+
+// Handle MIDI events
+
+function onMessage(deltaTime, message) {
+    if (message[0] === 0x80 || message[0] === 0x90 && message[2] === 0) {
+        console.log('Note Off: %d', message[1]);
+        if (stepperPlayer) stepperPlayer.off();
+    } else if (message[0] === 0x90) {
+        var freq = MIDIUtils.noteNumberToFrequency(message[1]);
+        var noteName = MIDIUtils.noteNumberToName(message[1]);
+        console.log('Note On:  %d (%s, %d Hz)', message[1], noteName, freq);
+        if (stepperPlayer) stepperPlayer.play(freq);
+    }
+}
+
+
+// MIDI port
+
+var input;
+if (typeof midiPort === "number") {
+    input = new midi.input();
+    if (input.getPortCount() < midiPort + 1) {
+        console.error('Invalid MIDI port: %d', midiPort);
+        process.exit(1);
+    }
+    input.openPort(midiPort);
+    input.ignoreTypes(false, false, false);
+    input.on('message', onMessage);
+    console.log('Listening on MIDI port %d (%s)', midiPort, input.getPortName(midiPort));
+}
+
+
+// Virtual MIDI port
+
+console.log('Listening on virtual MIDI port "%s"', midiName);
+var vinput = new midi.input();
+vinput.openVirtualPort(midiName);
+vinput.ignoreTypes(false, false, false);
+vinput.on('message', onMessage);
+
+
+// Output to StepperPlayer
+
+console.log('Connecting to StepperPlayer on %s', device);
+stepperPlayer = new StepperPlayer(device);
+stepperPlayer.on('connect', function () {
+    console.log('Connected to StepperPlayer');
+});
+stepperPlayer.on('error', function (err) {
+    console.error('StepperPlayer: ' + err.message);
+    stepperPlayer = false;
+});
+
+
+// Handle process signals
+
+process.on('SIGTERM', function () {
+    if (input) input.closePort();
+    vinput.closePort();
+
+    if (stepperPlayer) {
+        stepperPlayer.off();
+        stepperPlayer.close();
+        // TODO: stepperPlayer.reset()
+    }
+});
