b920255f609698946b65883723b32c8c7dfd8d7f
Nico Kaiser <nico@kaiser.me>
2014-09-02 08:27:38 +0200

    Add RTP-MIDI support

--- node-stepper-midi/midi-bridge.js
+++ node-stepper-midi/midi-bridge.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var midi = require('midi');
+var rtpmidi = require('rtpmidi');
 var MIDIUtils = require('midiutils');
 var StepperPlayer = require('./lib/stepper-player').StepperPlayer;
 
@@ -8,6 +9,8 @@ var StepperPlayer = require('./lib/stepper-player').StepperPlayer;
 // Configuration
 
 var midiPort = 1; // real MIDI port (or false)
+var midiName = 'Hardware Music'; // "Hardware Music"
+var rtpMidiPort = 5008; // 5008
 var device = '/dev/ttyACM0'; // /dev/ttyACM0
 
 
@@ -51,6 +54,22 @@ if (typeof midiPort === "number") {
 }
 
 
+// RTP-MIDI
+
+console.log('Listening on RTP-MIDI port %d (%s)', rtpMidiPort, midiName);
+var rtpMidiSession = rtpmidi.manager.createSession({
+    localName: midiName,
+    bonjourName: midiName,
+    port: rtpMidiPort
+});
+rtpMidiSession.on('message', function (deltaTime, message) {
+    //var commands = Array.prototype.slice.call(message, 0);
+    // ...
+    // TODO: onMessage(deltaTime, message);
+    console.log(message);
+});
+
+
 // Output to StepperPlayer
 
 console.log('Connecting to StepperPlayer on %s', device);
@@ -67,6 +86,7 @@ stepperPlayer.on('error', function (err) {
 
 process.on('SIGTERM', function () {
     if (input) input.closePort();
+    rtpMidiSession.close();
 
     stepperPlayer.off();
     stepperPlayer.close();
